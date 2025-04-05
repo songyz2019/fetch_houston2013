d783844ac91fc8c3a6d8cd4c520dc65d087249f5
songyz2019 <songyz2023dlut@outlook.com>
2025-04-05 18:01:25 +0800

    fix ci/cd

--- etc/bump-version.bash
+++ etc/bump-version.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export VERSION=$(uvx dunamai from git --bump --style pep440)
+export VERSION=$(uvx dunamai from git --bump --no-metadata --style pep440)
 uvx --from=toml-cli toml set --toml-path=pyproject.toml project.version $VERSION
